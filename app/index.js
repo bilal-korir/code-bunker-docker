05469950faf6a3449bed5e60f803d31e2ca48424
Bilal Korir <seekingk@live.com>
2021-09-25 12:24:17 +0800

    Build image

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(port, (port) => {
+  console.log(`Example app listening at http://localhost:3000`);
+});
